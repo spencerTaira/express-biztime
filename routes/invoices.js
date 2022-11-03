1fe8781fb3159f890ade8731b75c9f75d5686c11
Spencer Taira <spencermtaira@gmail.com>
2022-11-03 15:43:06 -0700

    added invoices routes up to POST

--- routes/invoices.js
+++ routes/invoices.js
@@ -10,7 +10,7 @@ const { NotFoundError, BadRequestError } = require("../expressError");
  *   }
  * }
  */
-router.get('/', async function (req, res) {
+router.get('/', async function(req, res) {
   const result = await db.query(
     `SELECT id, comp_code
       FROM invoices
@@ -26,7 +26,66 @@ router.get('/', async function (req, res) {
  * Returns an object of a given invoice with JSON object like
  * {
  *  invoice: {
- *    id, amt, paid, add_date, paid_date, company: {
- * code, name, description}}
+ *    id, amt, paid, add_date, paid_date,
+ *    company: {
+ *      code, name, description
+ *    }
+ *  }
+ * }
+ * If invoice cannot be found returns 404
  */
+
+router.get('/:id', async function(req, res) {
+  const id = req.params.id;
+
+  const iResult = await db.query(
+    `
+    SELECT id, amt, paid, add_date, paid_date
+      FROM invoices
+      WHERE id = $1
+    `, [id]
+  );
+
+  const invoice = iResult.rows[0];
+  if (!invoice) throw new NotFoundError(`Invoice id ${id} Not Found`);
+
+  const cResult = await db.query(
+    `
+    SELECT code, name, description
+      FROM invoices
+        JOIN companies ON comp_code = code
+      WHERE id = $1
+    `, [id]
+  );
+
+  invoice.company = cResult.rows[0];
+  return res.json({ invoice });
+
+});
+
+/**
+ * Adds a new record to the invoice table
+ *  Input: JSON Object
+ *    {comp_code, amt}
+ *  Output: JSON Object
+ *    {
+ *      invoice: {
+ *        id, comp_code, amt, paid, add_date, paid_date
+ *      }
+ *    }
+ */
+router.post('/', async function(req, res) {
+  const {comp_code, amt} = req.body;
+
+  const result = await db.query(
+    `
+    INSERT INTO invoices (comp_code, amt)
+    VALUES ($1, $2)
+    RETURNING id, comp_code, amt, paid, add_date, paid_date
+    `, [comp_code, amt]
+  );
+
+  const invoice = result.rows[0]
+  return res.json({ invoice });
+});
 module.exports = router;
